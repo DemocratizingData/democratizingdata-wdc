8bde19ae6c0ff3bf8d310a921b035b1282920cb7
Joon-Yee Chuah <jchuah@tacc.utexas.edu>
2022-08-08 14:50:53 -0500

    web data connector container

---
+++ wdc/src/content/wdc.js
@@ -0,0 +1,45 @@
+(function () {
+  var myConnector = tableau.makeConnector();
+
+  myConnector.getSchema = function (schemaCallback) {
+    var cols = [{
+      id: "id",
+      dataType: tableau.dataTypeEnum.string
+    }, {
+        id: "mag",
+        alias: "magnitude",
+        dataType: tableau.dataTypeEnum.float
+    }, {
+        id: "title",
+        alias: "title",
+        dataType: tableau.dataTypeEnum.string
+    }, {
+        id: "location",
+        dataType: tableau.dataTypeEnum.geometry
+    }];
+
+    var tableSchema = {
+        id: "earthquakeFeed",
+        alias: "Earthquakes with magnitude greater than 4.5 in the last seven days",
+        columns: cols
+    };
+
+    schemaCallback([tableSchema]);
+  };
+
+  myConnector.getData = function (table, doneCallback) {
+
+  };
+
+  tableau.registerConnector(myConnector);
+
+
+  $(document).ready(function () {
+    console.log("TABLEAU OBJECT", tableau);
+    $("#submitButton").click(function () {
+        tableau.connectionName = "USGS Earthquake Feed";
+
+        tableau.submit();
+    });
+});
+})();
--- wdc/src/index.js
+++ wdc/src/index.js
@@ -4,7 +4,7 @@ const finalhandler = require('finalhandler');
 const serveStatic = require('serve-static');
 
 // utilize the same pattern for exiting port and cache settings
-const serverPortNumber = process.env.SERVER_PORT || 8888;
+const serverPortNumber = process.env.SERVER_PORT || 8889;
 const args = process.argv.slice(2);
 const disableCache = Array.isArray(args) && args.includes('--no-cache');
 
@@ -18,8 +18,9 @@ const setCustomCacheControl = (res, path) => {
 };
 
 // serve root folder supporting relative path for accessing resources and cache settings
-const serve = serveStatic('./', {
+const serve = serveStatic('./content', {
   setHeaders: setCustomCacheControl,
+  index: [ 'index.html' ]
 });
 
 const httpServer = http.createServer((req, res) => {
@@ -27,5 +28,5 @@ const httpServer = http.createServer((req, res) => {
 });
 httpServer.listen(serverPortNumber);
 
-console.log(`[Simulator HTTP Server] serving at: http://localhost:${serverPortNumber.toString().trim()}/Simulator/index.html`);
-console.log(`[Simulator HTTP Server] disable serving resources with cache: ${disableCache}`);
+console.log(`[WDC HTTP Server] serving at: http://localhost:${serverPortNumber.toString().trim()}`);
+console.log(`[WDC HTTP Server] disable serving resources with cache: ${disableCache}`);
